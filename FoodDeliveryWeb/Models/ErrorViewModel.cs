218ebe3d1cd9740020e72f1454fbeaf880e52352
ayurkiv <yurkiv.andrew@gmail.com>
2023-10-25 21:32:51 +0300

    del

--- FoodDeliveryWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FoodDeliveryWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
