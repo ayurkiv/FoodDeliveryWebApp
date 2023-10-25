04ca8ed97e6ab48c92a110f4a105b4c79d3a30f5
ayurkiv <yurkiv.andrew@gmail.com>
2023-10-25 22:58:53 +0300

    day_2_tutorial

--- FoodDelivery.Models/Admin.cs
+++ FoodDelivery.Models/Admin.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace FoodDelivery.Models
 {
-    public class Admin
+    public class Admin : IdentityUser
     {
         public int Id { get; set; }
         public string Name { get; set; }
