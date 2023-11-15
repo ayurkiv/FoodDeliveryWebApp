a927e66178b385ae1c8bc3d82fec0f8b322bf390
ayurkiv <yurkiv.andrew@gmail.com>
2023-11-15 16:46:23 +0200

    ADD card-details

--- FoodDelivery/wwwroot/js/site.js
+++ FoodDelivery/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function openFoodItemModal(name, category, price, imageUrl, description) {
+    // Заповніть модальне вікно даними
+    document.getElementById('modalFoodItemImage').src = imageUrl;
+    document.getElementById('modalFoodItemName').innerText = name;
+    document.getElementById('modelFoodDescription').innerText = description;
+    document.getElementById('modalFoodItemCategory').innerText = category;
+    document.getElementById('modalFoodItemPrice').innerText = price + '$';
+
+    // Відкрийте модальне вікно
+    $('#foodItemModal').modal('show');
+}
