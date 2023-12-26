bb59330e334686f383febe0e3ab53ef30587540e
Denys Prokopiuk <prokopiuckdenys250904@gmail.com>
2023-12-26 20:06:58 +0200

    low state design manage

--- FoodDelivery/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ FoodDelivery/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -113,6 +113,7 @@ namespace  FoodDelivery.Areas.Identity.Pages.Account.Manage
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
+        
         public static string PageNavClass(ViewContext viewContext, string page)
         {
             var activePage = viewContext.ViewData["ActivePage"] as string
