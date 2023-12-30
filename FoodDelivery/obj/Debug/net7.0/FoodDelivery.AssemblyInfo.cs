5ceb353c7e707712b29ed9ca659b463f616e862a
Denys Prokopiuk <prokopiuckdenys250904@gmail.com>
2023-12-30 20:37:30 +0200

    Admin Products

--- FoodDelivery/obj/Debug/net7.0/FoodDelivery.AssemblyInfo.cs
+++ FoodDelivery/obj/Debug/net7.0/FoodDelivery.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodDelivery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24d3220259cd300b1b6ece7d439c453a638e2750")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodDelivery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodDelivery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
